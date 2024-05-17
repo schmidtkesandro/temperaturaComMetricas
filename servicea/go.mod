7978790869f93afc3231cfd2b910cfc71517111c
schmidtkesandro <sandroluis@terra.com.br>
2024-05-17 00:28:47 -0300

    atualização métricas

--- servicea/go.mod
+++ servicea/go.mod
@@ -2,4 +2,10 @@ module github.com/schmidtkesandro/temperaturaComMetricas
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.0
+	go.opentelemetry.io/otel v1.8.0
+	go.opentelemetry.io/otel/exporters/zipkin v1.8.0
+	go.opentelemetry.io/otel/sdk/trace v1.8.0
+	go.opentelemetry.io/otel/trace v1.8.0
+)
--- servicea/main.go
+++ servicea/main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/zipkin"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.8.0"
 )
 
+func initTracer(serviceName string) func() {
+	endpoint := "http://localhost:9411/api/v2/spans"
+
+	exporter, err := zipkin.New(
+		endpoint,
+	)
+	if err != nil {
+		log.Fatalf("failed to initialize Zipkin exporter %v", err)
+	}
+
+	bsp := trace.NewBatchSpanProcessor(exporter)
+	tracerProvider := trace.NewTracerProvider(
+		trace.WithSpanProcessor(bsp),
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(serviceName),
+		)),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	return func() {
+		if err := tracerProvider.Shutdown(context.Background()); err != nil {
+			log.Fatalf("failed to shut down tracer provider %v", err)
+		}
+	}
+}
+
 func main() {
+	// Service A
+	shutdown := initTracer("serviceA")
+	defer shutdown()
 	r := mux.NewRouter()
 	r.HandleFunc("/cep", handleCEP).Methods("POST")
 	r.HandleFunc("/home", homeHandler)
